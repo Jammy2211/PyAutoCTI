e93f7217a2794ad90362d6b71de9c03a892d4ee2
James Nightingale <james.w.nightingale@durham.ac.uk>
2019-01-31 18:19:32 +0000

    Added signal to noise map and max to CIData class

--- autocti/charge_injection/ci_data.py
+++ autocti/charge_injection/ci_data.py
@@ -64,6 +64,18 @@ class CIData(object):
     def parallel_serial_calibration_data(self):
         return self.map(lambda obj: obj.parallel_serial_calibration_section())
 
+    @property
+    def signal_to_noise_map(self):
+        """The estimated signal-to-noise_maps mappers of the image."""
+        signal_to_noise_map = np.divide(self.image, self.noise_map)
+        signal_to_noise_map[signal_to_noise_map < 0] = 0
+        return signal_to_noise_map
+
+    @property
+    def signal_to_noise_max(self):
+        """The maximum value of signal-to-noise_maps in an image pixel in the image's signal-to-noise_maps mappers"""
+        return np.max(self.signal_to_noise_map)
+
 
 class CIDataFit(object):
 
