482e4452446af72881824e033e8a8d8588b80962
James Nightingale <james.w.nightingale@durham.ac.uk>
2019-01-31 18:31:50 +0000

    Added ci_data plotters for individual attributes

--- autocti/charge_injection/plotters/data_plotters.py
+++ autocti/charge_injection/plotters/data_plotters.py
@@ -49,8 +49,31 @@ def plot_noise_map(noise_map, mask=None, as_subplot=False,
                               title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
                               xyticksize=xyticksize,
                               output_path=output_path, output_format=output_format, output_filename=output_filename)
-    
 
+def plot_ci_pre_cti(ci_pre_cti, mask=None, as_subplot=False,
+               figsize=(7, 7), aspect='equal',
+               cmap='jet', norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
+               cb_ticksize=10, cb_fraction=0.047, cb_pad=0.01,
+               title='Image', titlesize=16, xlabelsize=16, ylabelsize=16, xyticksize=16,
+               output_path=None, output_format='show', output_filename='ci_pre_cti'):
+    """Plot the observed ci_pre_cti of the ccd data.
+
+    Set *autocti.data.plotters.array_plotters* for a description of all input parameters not described below.
+
+    Parameters
+    -----------
+    ci_pre_cti : CIFrame
+        The ci_pre_cti of the data.
+    """
+    array_plotters.plot_array(array=ci_pre_cti, mask=mask, as_subplot=as_subplot,
+                              figsize=figsize, aspect=aspect,
+                              cmap=cmap, norm=norm, norm_min=norm_min, norm_max=norm_max,
+                              linthresh=linthresh, linscale=linscale,
+                              cb_ticksize=cb_ticksize, cb_fraction=cb_fraction, cb_pad=cb_pad,
+                              title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
+                              xyticksize=xyticksize,
+                              output_path=output_path, output_format=output_format, output_filename=output_filename)
+    
 def plot_signal_to_noise_map(signal_to_noise_map, mask=None, as_subplot=False,
                figsize=(7, 7), aspect='equal',
                cmap='jet', norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
--- test/charge_injection/plotters/test_data_plotters.py
+++ test/charge_injection/plotters/test_data_plotters.py
@@ -29,6 +29,10 @@ def make_data_plotter_setup():
 
     return data_plotter_path
 
+@pytest.fixture(name='mask')
+def make_mask():
+    return msk.Mask.empty_for_shape(shape=(6,6), frame_geometry=MockGeometry(), ci_pattern=MockPattern())
+
 @pytest.fixture(name='image')
 def make_image():
     return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=np.ones((6,6)))
@@ -37,9 +41,9 @@ def make_image():
 def make_noise_map():
     return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=2.0*np.ones((6,6)))
 
-@pytest.fixture(name='mask')
-def make_mask():
-    return msk.Mask.empty_for_shape(shape=(6,6), frame_geometry=MockGeometry(), ci_pattern=MockPattern())
+@pytest.fixture(name='ci_pre_cti')
+def make_ci_pre_cti():
+    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=3.0*np.ones((6,6)))
 
 
 def test__image_is_output(image, mask, data_plotter_path):
@@ -53,6 +57,12 @@ def test__noise_map_is_output(noise_map, mask, data_plotter_path):
     data_plotters.plot_noise_map(noise_map=noise_map, mask=mask, output_path=data_plotter_path, output_format='png')
     assert os.path.isfile(path=data_plotter_path+'noise_map.png')
     os.remove(path=data_plotter_path+'noise_map.png')
+
+def test__ci_pre_cti_is_output(ci_pre_cti, mask, data_plotter_path):
+
+    data_plotters.plot_ci_pre_cti(ci_pre_cti=ci_pre_cti, mask=mask, output_path=data_plotter_path, output_format='png')
+    assert os.path.isfile(path=data_plotter_path+'ci_pre_cti.png')
+    os.remove(path=data_plotter_path+'ci_pre_cti.png')
 
 def test__signal_to_noise_map_is_output(image, noise_map, mask, data_plotter_path):
 
