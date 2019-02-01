74a003e913fac057e86af61781888ed218e54787
Richard Hayes <richardhayes777@gmail.com>
2019-02-01 09:39:03 +0000

    fixed path creation in tests

--- test/charge_injection/plotters/test_data_plotters.py
+++ test/charge_injection/plotters/test_data_plotters.py
@@ -1,22 +1,22 @@
 import os
+import pathlib
 import shutil
 
-import pytest
 import numpy as np
-
+import pytest
 from autofit import conf
-from autocti.data import mask as msk
-from autocti.charge_injection import ci_frame
-from autocti.charge_injection import ci_data
-from autocti.charge_injection.plotters import data_plotters
 
+from autocti.charge_injection import ci_frame
+from autocti.charge_injection.plotters import data_plotters
+from autocti.data import mask as msk
 from test.mock.mock import MockGeometry, MockPattern
 
 
 @pytest.fixture(name='general_config')
 def make_general_config():
     general_config_path = "{}/../../test_files/configs/plotting/".format(os.path.dirname(os.path.realpath(__file__)))
-    conf.instance.general = conf.NamedConfig(general_config_path+"general.ini")
+    conf.instance.general = conf.NamedConfig(general_config_path + "general.ini")
+
 
 @pytest.fixture(name='data_plotter_path')
 def make_data_plotter_setup():
@@ -25,48 +25,52 @@ def make_data_plotter_setup():
     if os.path.exists(data_plotter_path):
         shutil.rmtree(data_plotter_path)
 
-    os.mkdir(data_plotter_path)
+    pathlib.Path(data_plotter_path).mkdir(parents=True, exist_ok=True)
 
     return data_plotter_path
 
+
 @pytest.fixture(name='mask')
 def make_mask():
-    return msk.Mask.empty_for_shape(shape=(6,6), frame_geometry=MockGeometry(), ci_pattern=MockPattern())
+    return msk.Mask.empty_for_shape(shape=(6, 6), frame_geometry=MockGeometry(), ci_pattern=MockPattern())
+
 
 @pytest.fixture(name='image')
 def make_image():
-    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=np.ones((6,6)))
+    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=np.ones((6, 6)))
+
 
 @pytest.fixture(name='noise_map')
 def make_noise_map():
-    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=2.0*np.ones((6,6)))
+    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=2.0 * np.ones((6, 6)))
+
 
 @pytest.fixture(name='ci_pre_cti')
 def make_ci_pre_cti():
-    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=3.0*np.ones((6,6)))
+    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=3.0 * np.ones((6, 6)))
 
 
 def test__image_is_output(image, mask, data_plotter_path):
-
     data_plotters.plot_image(image=image, mask=mask, output_path=data_plotter_path, output_format='png')
-    assert os.path.isfile(path=data_plotter_path+'image.png')
-    os.remove(path=data_plotter_path+'image.png')
+    assert os.path.isfile(path=data_plotter_path + 'image.png')
+    os.remove(path=data_plotter_path + 'image.png')
+
 
 def test__noise_map_is_output(noise_map, mask, data_plotter_path):
-
     data_plotters.plot_noise_map(noise_map=noise_map, mask=mask, output_path=data_plotter_path, output_format='png')
-    assert os.path.isfile(path=data_plotter_path+'noise_map.png')
-    os.remove(path=data_plotter_path+'noise_map.png')
+    assert os.path.isfile(path=data_plotter_path + 'noise_map.png')
+    os.remove(path=data_plotter_path + 'noise_map.png')
+
 
 def test__ci_pre_cti_is_output(ci_pre_cti, mask, data_plotter_path):
-
     data_plotters.plot_ci_pre_cti(ci_pre_cti=ci_pre_cti, mask=mask, output_path=data_plotter_path, output_format='png')
-    assert os.path.isfile(path=data_plotter_path+'ci_pre_cti.png')
-    os.remove(path=data_plotter_path+'ci_pre_cti.png')
+    assert os.path.isfile(path=data_plotter_path + 'ci_pre_cti.png')
+    os.remove(path=data_plotter_path + 'ci_pre_cti.png')
+
 
 def test__signal_to_noise_map_is_output(image, noise_map, mask, data_plotter_path):
-
-    data_plotters.plot_signal_to_noise_map(signal_to_noise_map=image/noise_map, mask=mask, output_path=data_plotter_path,
-                                          output_format='png')
-    assert os.path.isfile(path=data_plotter_path+'signal_to_noise_map.png')
-    os.remove(path=data_plotter_path+'signal_to_noise_map.png')
+    data_plotters.plot_signal_to_noise_map(signal_to_noise_map=image / noise_map, mask=mask,
+                                           output_path=data_plotter_path,
+                                           output_format='png')
+    assert os.path.isfile(path=data_plotter_path + 'signal_to_noise_map.png')
+    os.remove(path=data_plotter_path + 'signal_to_noise_map.png')
--- test/charge_injection/plotters/test_fit_plotters.py
+++ test/charge_injection/plotters/test_fit_plotters.py
@@ -1,25 +1,26 @@
 import os
+import pathlib
 import shutil
 
-import pytest
 import numpy as np
-
+import pytest
 from autofit import conf
-from autocti.data import mask as msk
-from autocti.charge_injection import ci_frame
+
 from autocti.charge_injection import ci_data
 from autocti.charge_injection import ci_fit
+from autocti.charge_injection import ci_frame
 from autocti.charge_injection.plotters import fit_plotters
+from autocti.data import mask as msk
 from autocti.model import arctic_params
 from autocti.model import arctic_settings
-
 from test.mock.mock import MockGeometry, MockPattern
 
 
 @pytest.fixture(name='general_config')
 def make_general_config():
     general_config_path = "{}/../../test_files/configs/plotting/".format(os.path.dirname(os.path.realpath(__file__)))
-    conf.instance.general = conf.NamedConfig(general_config_path+"general.ini")
+    conf.instance.general = conf.NamedConfig(general_config_path + "general.ini")
+
 
 @pytest.fixture(name='fit_path')
 def make_fit_setup():
@@ -28,29 +29,36 @@ def make_fit_setup():
     if os.path.exists(fit_path):
         shutil.rmtree(fit_path)
 
-    os.mkdir(fit_path)
+    pathlib.Path(fit_path).mkdir(parents=True, exist_ok=True)
 
     return fit_path
 
+
 @pytest.fixture(name='mask')
 def make_mask():
-    return msk.Mask.empty_for_shape(shape=(6,6), frame_geometry=MockGeometry(), ci_pattern=MockPattern())
+    return msk.Mask.empty_for_shape(shape=(6, 6), frame_geometry=MockGeometry(), ci_pattern=MockPattern())
+
 
 @pytest.fixture(name='image')
 def make_image():
-    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=np.ones((6,6)))
+    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=np.ones((6, 6)))
+
 
 @pytest.fixture(name='noise_map')
 def make_noise_map():
-    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=2.0*np.ones((6,6)))
+    return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=2.0 * np.ones((6, 6)))
+
 
 @pytest.fixture(name='ci_pre_cti')
 def make_ci_pre_cti():
-    return ci_data.CIPreCTI(frame_geometry=ci_frame.QuadGeometryEuclid.bottom_left(), ci_pattern=MockPattern(), array=3.0*np.ones((6,6)))
+    return ci_data.CIPreCTI(frame_geometry=ci_frame.QuadGeometryEuclid.bottom_left(), ci_pattern=MockPattern(),
+                            array=3.0 * np.ones((6, 6)))
+
 
 @pytest.fixture(name='ci_data')
 def make_ci_data(image, noise_map, ci_pre_cti):
     return ci_data.CIData(image=image, noise_map=noise_map, ci_pre_cti=ci_pre_cti, noise_scalings=None)
+
 
 @pytest.fixture(name='ci_datas_fit')
 def make_ci_datas_fit(ci_data, mask):
@@ -58,6 +66,7 @@ def make_ci_datas_fit(ci_data, mask):
     ci_datas_fit.append(ci_data)
     ci_datas_fit[0].mask = mask
     return ci_datas_fit
+
 
 @pytest.fixture(name='cti_params')
 def make_cti_params():
@@ -68,6 +77,7 @@ def make_cti_params():
 
     return arctic_params.ArcticParams(parallel_species=[parallel_species], parallel_ccd=parallel_ccd)
 
+
 @pytest.fixture(name='cti_settings')
 def make_cti_settings():
     parallel_settings = arctic_settings.Settings(well_depth=84700, niter=1, express=5, n_levels=2000,
@@ -75,12 +85,13 @@ def make_cti_settings():
 
     return arctic_settings.ArcticSettings(neomode='NEO', parallel=parallel_settings)
 
+
 @pytest.fixture(name='ci_fit')
 def make_ci_fit(ci_datas_fit, cti_params, cti_settings):
     return ci_fit.CIFit(ci_datas_fit=ci_datas_fit, cti_params=cti_params, cti_settings=cti_settings)
 
-def test__image_is_output(ci_fit, fit_path):
 
+def test__image_is_output(ci_fit, fit_path):
     fit_plotters.plot_image(fit=ci_fit, fit_index=0, output_path=fit_path, output_format='png')
     assert os.path.isfile(path=fit_path + 'fit_image.png')
     os.remove(path=fit_path + 'fit_image.png')
