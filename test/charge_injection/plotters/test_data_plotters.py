f3753a74a25a586189e6ef1263d9b97b7152646f
Richard Hayes <richardhayes777@gmail.com>
2019-02-01 12:38:19 +0000

    using patching for all potter tests

--- test/charge_injection/plotters/test_data_plotters.py
+++ test/charge_injection/plotters/test_data_plotters.py
@@ -1,33 +1,15 @@
-import os
-import pathlib
-import shutil
-
 import numpy as np
-import pytest
-from autofit import conf
 
 from autocti.charge_injection import ci_frame
 from autocti.charge_injection.plotters import data_plotters
 from autocti.data import mask as msk
+from test.charge_injection.plotters.fixtures import *
 from test.mock.mock import MockGeometry, MockPattern
-
-
-@pytest.fixture(name='general_config')
-def make_general_config():
-    general_config_path = "{}/../../test_files/configs/plotting/".format(os.path.dirname(os.path.realpath(__file__)))
-    conf.instance.general = conf.NamedConfig(general_config_path + "general.ini")
 
 
 @pytest.fixture(name='data_plotter_path')
 def make_data_plotter_setup():
-    data_plotter_path = "{}/../../test_files/plotting/data/".format(os.path.dirname(os.path.realpath(__file__)))
-
-    if os.path.exists(data_plotter_path):
-        shutil.rmtree(data_plotter_path)
-
-    pathlib.Path(data_plotter_path).mkdir(parents=True, exist_ok=True)
-
-    return data_plotter_path
+    return "{}/../../test_files/plotting/data/".format(os.path.dirname(os.path.realpath(__file__)))
 
 
 @pytest.fixture(name='mask')
@@ -50,27 +32,23 @@ def make_ci_pre_cti():
     return ci_frame.CIFrame(frame_geometry=MockGeometry(), ci_pattern=MockPattern(), array=3.0 * np.ones((6, 6)))
 
 
-def test__image_is_output(image, mask, data_plotter_path):
+def test__image_is_output(image, mask, data_plotter_path, plot_patch):
     data_plotters.plot_image(image=image, mask=mask, output_path=data_plotter_path, output_format='png')
-    assert os.path.isfile(path=data_plotter_path + 'image.png')
-    os.remove(path=data_plotter_path + 'image.png')
+    assert data_plotter_path + 'image.png' in plot_patch.paths
 
 
-def test__noise_map_is_output(noise_map, mask, data_plotter_path):
+def test__noise_map_is_output(noise_map, mask, data_plotter_path, plot_patch):
     data_plotters.plot_noise_map(noise_map=noise_map, mask=mask, output_path=data_plotter_path, output_format='png')
-    assert os.path.isfile(path=data_plotter_path + 'noise_map.png')
-    os.remove(path=data_plotter_path + 'noise_map.png')
+    assert data_plotter_path + 'noise_map.png' in plot_patch.paths
 
 
-def test__ci_pre_cti_is_output(ci_pre_cti, mask, data_plotter_path):
+def test__ci_pre_cti_is_output(ci_pre_cti, mask, data_plotter_path, plot_patch):
     data_plotters.plot_ci_pre_cti(ci_pre_cti=ci_pre_cti, mask=mask, output_path=data_plotter_path, output_format='png')
-    assert os.path.isfile(path=data_plotter_path + 'ci_pre_cti.png')
-    os.remove(path=data_plotter_path + 'ci_pre_cti.png')
+    assert data_plotter_path + 'ci_pre_cti.png' in plot_patch.paths
 
 
-def test__signal_to_noise_map_is_output(image, noise_map, mask, data_plotter_path):
+def test__signal_to_noise_map_is_output(image, noise_map, mask, data_plotter_path, plot_patch):
     data_plotters.plot_signal_to_noise_map(signal_to_noise_map=image / noise_map, mask=mask,
                                            output_path=data_plotter_path,
                                            output_format='png')
-    assert os.path.isfile(path=data_plotter_path + 'signal_to_noise_map.png')
-    os.remove(path=data_plotter_path + 'signal_to_noise_map.png')
+    assert data_plotter_path + 'signal_to_noise_map.png' in plot_patch.paths
