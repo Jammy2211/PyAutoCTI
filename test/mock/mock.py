1c3f1b711b6d5afc2d4976ef76272ff598dba9ec
Richard Hayes <richardhayes777@gmail.com>
2019-02-04 09:49:52 +0000

    removed set_region_on_array_to_zeros; replaced MockRegion with Region

--- test/mock/mock.py
+++ test/mock/mock.py
@@ -1,21 +1,5 @@
 import numpy as np
-
-
-class MockRegion(tuple):
-
-    def __new__(cls, region):
-        region = super(MockRegion, cls).__new__(cls, region)
-
-        region.y0 = region[0]
-        region.y1 = region[1]
-        region.x0 = region[2]
-        region.x1 = region[3]
-
-        return region
-
-    def set_region_on_array_to_zeros(self, array):
-        array[self.y0:self.y1, self.x0:self.x1] = 0.0
-        return array
+from autocti.charge_injection.ci_frame import Region
 
 
 class MockPattern(object):
@@ -34,8 +18,8 @@ class MockCIGeometry(object):
 
     def __init__(self, serial_prescan=(0, 1, 0, 1), serial_overscan=(0, 1, 0, 1)):
         super(MockCIGeometry, self).__init__()
-        self.serial_prescan = MockRegion(serial_prescan)
-        self.serial_overscan = MockRegion(serial_overscan)
+        self.serial_prescan = Region(serial_prescan)
+        self.serial_overscan = Region(serial_overscan)
 
 
 class MockCIPreCTI(np.ndarray):
