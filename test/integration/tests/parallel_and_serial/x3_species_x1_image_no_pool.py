a7661648f17304a5e5a9314f39068f3b069d2b19
James Nightingale <james.w.nightingale@durham.ac.uk>
2019-02-05 13:00:51 +0000

    Integration tests now working for all CTI directions.

--- test/integration/tests/parallel_and_serial/x3_species_x1_image_no_pool.py
+++ test/integration/tests/parallel_and_serial/x3_species_x1_image_no_pool.py
@@ -1,12 +1,12 @@
 import os
 
 from autofit import conf
-from autofit.optimize import non_linear as nl
 from autofit.mapper import prior_model
+from autofit.optimize import non_linear as nl
+
 from autocti.charge_injection import ci_data, ci_pattern
 from autocti.model import arctic_params
 from autocti.model import arctic_settings
-
 from autocti.pipeline import phase as ph
 from autocti.pipeline import pipeline as pl
 from test.integration import tools
@@ -17,25 +17,34 @@ normalizations = [84700.0]
 frame_geometry = tools.CIQuadGeometryIntegration()
 
 test_type = 'parallel_and_serial'
-test_name = 'one_species'
+test_name = 'x3_species_x1_image_no_pool'
 
 path = '{}/../../'.format(os.path.dirname(os.path.realpath(__file__)))
-output_path = path+'output/'+test_type
-config_path = path+'config'
+output_path = path + 'output/' + test_type
+config_path = path + 'config'
 conf.instance = conf.Config(config_path=config_path, output_path=output_path)
+
 
 def pipeline():
 
-    parallel_species = arctic_params.Species(trap_density=0.1, trap_lifetime=1.5)
+    tools.reset_paths(test_name=test_name, output_path=output_path)
+
+    parallel_species_0 = arctic_params.Species(trap_density=0.5, trap_lifetime=2.0)
+    parallel_species_1 = arctic_params.Species(trap_density=1.5, trap_lifetime=5.0)
+    parallel_species_2 = arctic_params.Species(trap_density=2.5, trap_lifetime=20.0)
     parallel_ccd = arctic_params.CCD(well_notch_depth=0.01, well_fill_alpha=0.2,
                                      well_fill_beta=0.8, well_fill_gamma=2.0)
 
-    serial_species = arctic_params.Species(trap_density=0.1, trap_lifetime=1.5)
+    serial_species_0 = arctic_params.Species(trap_density=0.5, trap_lifetime=2.0)
+    serial_species_1 = arctic_params.Species(trap_density=1.5, trap_lifetime=5.0)
+    serial_species_2 = arctic_params.Species(trap_density=2.5, trap_lifetime=20.0)
     serial_ccd = arctic_params.CCD(well_notch_depth=0.01, well_fill_alpha=0.2,
                                      well_fill_beta=0.8, well_fill_gamma=2.0)
 
-    cti_params = arctic_params.ArcticParams(parallel_ccd=parallel_ccd, parallel_species=[parallel_species],
-                                            serial_ccd=serial_ccd, serial_species=[serial_species])
+    cti_params = arctic_params.ArcticParams(parallel_ccd=parallel_ccd,
+                                            parallel_species=[parallel_species_0, parallel_species_1, parallel_species_2],
+                                            serial_ccd=serial_ccd,
+                                            serial_species=[serial_species_0, serial_species_1, serial_species_2])
 
     parallel_settings = arctic_settings.Settings(well_depth=84700, niter=1, express=1, n_levels=2000,
                                                  charge_injection_mode=True, readout_offset=0)
@@ -43,13 +52,13 @@ def pipeline():
                                                  charge_injection_mode=False, readout_offset=0)
     cti_settings = arctic_settings.ArcticSettings(parallel=parallel_settings, serial=serial_settings)
 
-    tools.reset_paths(test_name=test_name, output_path=output_path)
-    tools.simulate_integration_quadrant(test_name=test_name, cti_params=cti_params, cti_settings=cti_settings)
+    tools.simulate_integration_quadrant(test_name=test_name, normalizations=normalizations, cti_params=cti_params,
+                                        cti_settings=cti_settings)
 
     pattern = ci_pattern.CIPatternUniform(normalization=normalizations[0], regions=ci_regions)
 
     data = ci_data.load_ci_data(frame_geometry=frame_geometry, ci_pattern=pattern,
-                                ci_image_path=path+'/data/'+test_name+'/ci_data_0.fits',
+                                ci_image_path=path + '/data/' + test_name + '/ci_data_0.fits',
                                 ci_noise_map_from_single_value=1.0,
                                 ci_pre_cti_from_image=True)
 
@@ -69,9 +78,13 @@ def make_pipeline(test_name):
             self.serial_ccd.well_fill_gamma = 0.0
 
     phase1 = ParallelSerialPhase(optimizer_class=nl.MultiNest,
-                                 parallel_species=[prior_model.PriorModel(arctic_params.Species)],
+                                 parallel_species=[prior_model.PriorModel(arctic_params.Species),
+                                                   prior_model.PriorModel(arctic_params.Species),
+                                                   prior_model.PriorModel(arctic_params.Species)],
                                  parallel_ccd=arctic_params.CCD,
-                                 serial_species=[prior_model.PriorModel(arctic_params.Species)],
+                                 serial_species=[prior_model.PriorModel(arctic_params.Species),
+                                                 prior_model.PriorModel(arctic_params.Species),
+                                                 prior_model.PriorModel(arctic_params.Species)],
                                  serial_ccd=arctic_params.CCD, phase_name="{}/phase1".format(test_name))
 
     phase1.optimizer.n_live_points = 60
