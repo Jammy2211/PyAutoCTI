61a91cfc22dbd4a3b70efcc9cd5f00b48c538eec
Richard Hayes <richardhayes777@gmail.com>
2019-03-12 10:49:30 +0000

    using autofit's pipeline functionality

--- autocti/pipeline/pipeline.py
+++ autocti/pipeline/pipeline.py
@@ -1,17 +1,13 @@
 import logging
 
 from autofit.tools import pipeline
-from autofit.tools.phase import ResultsCollection
 
 logger = logging.getLogger(__name__)
 
 
 class Pipeline(pipeline.Pipeline):
-
     def run(self, ci_datas, cti_settings, pool=None):
-        results = []
-        for i, phase in enumerate(self.phases):
-            logger.info("Running Phase {} (Number {})".format(phase.phase_name, i))
-            results.append(phase.run(ci_datas=ci_datas, cti_settings=cti_settings,
-                                     results=ResultsCollection(results), pool=pool))
-        return results
+        def runner(phase, results):
+            return phase.run(ci_datas=ci_datas, cti_settings=cti_settings, results=results, pool=pool)
+
+        return self.run_function(runner)
