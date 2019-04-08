cf8de99132d5fb8cd1e9d37b17436a6f2edd506e
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-04-08 10:00:45 +0100

    Added tagging module with tags for columsn, rows, cosmic ray buffers.

---
+++ autocti/pipeline/tagging.py
@@ -0,0 +1,59 @@
+def phase_tag_from_phase_settings(columns, rows, include_cosmic_ray_tag, cosmic_ray_parallel_buffer,
+                                  cosmic_ray_serial_buffer, cosmic_ray_diagonal_buffer):
+
+    columns_tag = columns_tag_from_columns(columns=columns)
+    rows_tag = rows_tag_from_rows(rows=rows)
+
+    cosmic_ray_buffer_tag = cosmic_ray_buffer_tag_from_cosmic_ray_buffers(include_cosmic_ray_tag=include_cosmic_ray_tag,
+               cosmic_ray_parallel_buffer=cosmic_ray_parallel_buffer, cosmic_ray_serial_buffer=cosmic_ray_serial_buffer,
+               cosmic_ray_diagonal_buffer=cosmic_ray_diagonal_buffer)
+
+    return columns_tag + rows_tag + cosmic_ray_buffer_tag
+
+def columns_tag_from_columns(columns):
+    """Generate a columns tag, to customize phase names based on the number of columns of data extracted in the fit,
+    which is used to speed up parallel CTI fits.
+
+    This changes the phase name 'phase_name' as follows:
+
+    columns = None -> phase_name
+    columns = 10 -> phase_name_col_10
+    columns = 60 -> phase_name_col_60
+    """
+    if columns == None:
+        return ''
+    else:
+        return '_col_' + str(int(columns))
+    
+def rows_tag_from_rows(rows):
+    """Generate a rows tag, to customize phase names based on the number of rows of data extracted in the fit,
+    which is used to speed up serial CTI fits.
+
+    This changes the phase name 'phase_name' as follows:
+
+    rows = None -> phase_name
+    rows = (0, 10) -> phase_name_rows_(0,10)
+    rows = (20, 60) -> phase_name_rows_(20,60)
+    """
+    if rows == None:
+        return ''
+    else:
+        x0 = str(rows[0])
+        x1 = str(rows[1])
+        return ('_rows_(' + x0 + ',' + x1 + ')')
+
+def cosmic_ray_buffer_tag_from_cosmic_ray_buffers(include_cosmic_ray_tag, cosmic_ray_parallel_buffer,
+                                                  cosmic_ray_serial_buffer, cosmic_ray_diagonal_buffer):
+    """Generate a cosmic ray buffer tag, to customize phase names based on the size of the cosmic ray masks in the \
+    parallel, serial and diagonal directions
+
+    This changes the phase name 'phase_name' as follows:
+
+    cosmic_ray_parallel_buffer = 1, cosmic_ray_serial_buffer=2, cosmic_ray_diagonal_buffer=3 = -> phase_name_cr_p1s2d3
+    cosmic_ray_parallel_buffer = 10, cosmic_ray_serial_buffer=5, cosmic_ray_diagonal_buffer=1 = -> phase_name_cr_p10s5d1
+    """
+    if not include_cosmic_ray_tag:
+        return ''
+    elif include_cosmic_ray_tag:
+        return ('_cr_p' + str(cosmic_ray_parallel_buffer) + 's' + str(cosmic_ray_serial_buffer) + 'd'
+                + str(cosmic_ray_diagonal_buffer))
