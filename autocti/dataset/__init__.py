2fdb67f9b197022302d58f597c03237bc0a2afc2
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-07-24 19:04:23 +0100

    dataset classes now use abstract classes

--- autocti/dataset/__init__.py
+++ autocti/dataset/__init__.py
@@ -2,5 +2,5 @@ from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
 
-from autocti.dataset import preprocess
+from autoarray.dataset import preprocess
 from .imaging import Imaging, MaskedImaging
