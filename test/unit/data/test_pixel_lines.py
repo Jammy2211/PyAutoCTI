c381768f99f1e701781b292151df03d477f620b2
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2020-09-05 11:54:02 +0100

    Add line collection from data test

--- test/unit/data/test_pixel_lines.py
+++ test/unit/data/test_pixel_lines.py
@@ -53,3 +53,36 @@ class TestPixelLineCollection:
         assert lines.origins == ["a", "a", "b", "b"]
 
         assert lines.dates == [None, None, None, None]
+
+    def test__pixel_line_collection__init_from_data(self):
+        line_1 = PixelLine(data=[1, 2, 3], origin="a")
+        line_2 = PixelLine(data=[4, 5, 6], origin="a")
+        line_3 = PixelLine(data=[7, 8, 9], origin="b")
+        line_4 = PixelLine(data=[0, 0, 0], origin="b")
+
+        lines = PixelLineCollection(
+            data=[[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 0, 0]],
+            origins=["a", "a", "b", "b"],
+        )
+
+        assert lines.n_lines == 4
+
+        assert lines.data[0] == line_1.data
+        assert lines.data[1] == line_2.data
+        assert lines.data[2] == line_3.data
+        assert lines.data[3] == line_4.data
+
+        assert lines.lines[0].data == line_1.data
+        assert lines.lines[1].data == line_2.data
+        assert lines.lines[2].data == line_3.data
+        assert lines.lines[3].data == line_4.data
+
+        assert lines.lines[0].origin == line_1.origin
+        assert lines.lines[1].origin == line_2.origin
+        assert lines.lines[2].origin == line_3.origin
+        assert lines.lines[3].origin == line_4.origin
+
+        assert lines.lines[0].date is None
+        assert lines.lines[1].date is None
+        assert lines.lines[2].date is None
+        assert lines.lines[3].date is None
