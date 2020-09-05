bbabc8b1e19a19adf525d09b6cb2f3cbb3097b7c
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2020-09-05 11:28:03 +0100

    Add collection of pixel lines class

--- autocti/data/pixel_lines.py
+++ autocti/data/pixel_lines.py
@@ -14,8 +14,8 @@ class PixelLine(object):
     ):
         """ A 1D line of pixels (e.g. a single CTI trail) with metadata.
         
-        Or e.g. a stacked average of many lines, in which case the metadata 
-        parameters may be e.g. the average value or minimum value of a bin.
+        Or could be an averaged stack of many lines, in which case the metadata 
+        parameters may be e.g. the average value or the minimum value of a bin.
         
         Parameters
         ----------
@@ -67,3 +67,57 @@ class PixelLine(object):
             return len(self.data)
         else:
             return None
+
+
+class PixelLineCollection(object):
+    def __init__(self, lines):
+        """ A collection of 1D lines of pixels with metadata.
+        
+        Enables convenient analysis e.g. binning and stacking of CTI trails.
+        
+        Parameters
+        ----------
+        lines : [PixelLine]
+            A list of the PixelLine objects.
+            
+        Attributes
+        ----------
+        data : [[float]] 
+            The pixel counts of each line, in units of electrons.
+            
+        origins : [str]
+            The identifiers for the origins (e.g. image name) of each line.
+            
+        locations : [[int, int]] 
+            The row and column indices of the first pixel in the line in the 
+            image, for each line.
+            
+        dates : [float] 
+            The Julian date of each line.
+            
+        backgrounds : [float]
+            The background charge count of each line, in units of electrons.
+            
+        fluxes : [float]
+            The maximum charge in each line, in units of electrons.
+        
+        lengths : [int] 
+            The number of pixels in the data array of each line.
+            
+        n_lines : int 
+            The number of lines in the collection.
+        """
+        # Extract the attributes from each line object
+        self.lines = lines
+
+        # Extract the attributes from each line
+        self.data = [line.data for line in lines]
+        self.origins = [line.origin for line in lines]
+        self.locations = [line.location for line in lines]
+        self.dates = [line.date for line in lines]
+        self.backgrounds = [line.background for line in lines]
+        self.fluxes = [line.flux for line in lines]
+
+    @property
+    def n_lines(self):
+        return len(self.lines)
