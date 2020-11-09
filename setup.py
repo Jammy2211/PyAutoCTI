f70426e0a2e6d42b62a645f9cffdb305e4efd9ba
James Nightingale <james.w.nightingale@durham.ac.uk>
2020-11-09 15:56:27 +0000

    install files

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from codecs import open
 from os.path import abspath, dirname, join
 from subprocess import call
 
-from autocti import ``.ersion__
-from setuptools import Command, find_packages, setup
+from setuptools import find_packages, setup, Command
 
-this_dir = abspath(dirname(__file__))
-with open(join(this_dir, "README.md"), encoding="utf-8") as file:
-    long_description = file.read()
+
+def version():
+    with open("autocti/__init__.py") as f:
+        lines = f.read().split("\n")
+    for line in lines:
+        if "__version__" in line:
+            return line.split("=")[1].strip(" '\”")
 
 
 class RunTests(Command):
@@ -24,13 +27,19 @@ class RunTests(Command):
 
     def run(self):
         """Run all tests!"""
-        errno = call(["py.test_autocti", "--cov=autocti", "--cov-report=term-missing"])
+        errno = call(["py.test", "--cov=autocti", "--cov-report=term-missing"])
         raise SystemExit(errno)
 
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, "README.md"), encoding="utf-8") as file:
+    long_description = file.read()
+
+with open(join(this_dir, "requirements.txt")) as f:
+    requirements = f.read().split("\n")
 
 setup(
     name="autocti",
-    version=__version__,
+    version=version(),
     description="Automated Charge Transfer Inefficiency Modeling",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -54,25 +63,7 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     keywords="cli",
-    packages=find_packages(
-        exclude=[
-            "docs",
-            "tests*",
-            "autocti_workspace",
-            "autocti_workspace/*",
-            "autocti_workspace_jam",
-            "worspace_jam/*",
-        ]
-    ),
-    install_requires=[
-        "numpy",
-        "scipy",
-        "astropy",
-        "matplotlib",
-        "pymultinest",
-        "getDist",
-        "autofit==0.27.3",
-    ],
-    extras_require={"test_autocti": ["coverage", "pytest", "pytest-cov"]},
-    cmdclass={"test_autocti": RunTests},
+    install_requires=requirements,
+    extras_require={"test": ["coverage", "pytest", "pytest-cov"]},
+    cmdclass={"test": RunTests},
 )
