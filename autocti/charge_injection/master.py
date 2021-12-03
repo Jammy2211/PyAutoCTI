56d0d85cbd976152e916c0927023cc626eef0379
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-12-03 16:08:03 +0000

    simple method to estimate master ci image

---
+++ autocti/charge_injection/master.py
@@ -0,0 +1,33 @@
+from typing import List
+
+from autoarray.structures.arrays.two_d.array_2d import Array2D
+
+
+def master_ci_from(ci_list: List[Array2D]) -> Array2D:
+    """
+    Determine the master charge injection frame from a list of observations of charge injection images, which all use
+    the same charge injection parameters and therefore should all be identical except for read noise.
+
+    The master frame is computed by summing all images in the list and dividing by the number of images, e.g. it is
+    the mean value across the images in every pixel.
+
+    There are many effects this function does not account for (bias, NL, cosmics, CTI, etc.) that will most likely
+    need to be accounted for before a more realistic implementation is possible.
+
+    Parameters
+    ----------
+    ci_list
+        A list of charge injection images all of which are taken using the same charge injection
+        parameters / electronics.
+
+    Returns
+    -------
+    The estimated master charge injection image.
+
+    """
+
+    master_ci = sum(ci_list) / len(ci_list)
+
+    return Array2D.manual_native(
+        array=master_ci.native, pixel_scales=ci_list[0].pixel_scales
+    )
---
+++ test_autocti/charge_injection/test_master.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pytest
+
+import autocti as ac
+
+from autocti.charge_injection.master import master_ci_from
+
+
+def test__master_ci_from():
+
+    ci_0 = ac.Array2D.manual_native(array=[[1.0, 1.0], [2.0, 2.0]], pixel_scales=1.0)
+    ci_1 = ac.Array2D.manual_native(array=[[2.0, 2.0], [4.0, 4.0]], pixel_scales=1.0)
+
+    master_ci = master_ci_from(ci_list=[ci_0, ci_1])
+
+    assert master_ci.native == pytest.approx(np.array([[1.5, 1.5], [3.0, 3.0]]), 1.0e-4)
