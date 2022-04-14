de9932c17ea43a28ebd7a4657e7ac26eb916aaa6
James Nightingale <james.w.nightingale@durham.ac.uk>
2022-04-14 14:13:02 +0100

    test works

--- test_autocti/clocker/test_dict.py
+++ test_autocti/clocker/test_dict.py
@@ -1,7 +1,6 @@
 import os
 
 import pytest
-
 from autocti import Clocker2D
 
 
@@ -10,44 +9,45 @@ from autocti import Clocker2D
 )
 def make_clocker_dict():
     return {
-        'euclid_orientation_hack': False,
+        'type': 'autocti.clocker.two_d.Clocker2D',
         'iterations': 5,
-        'parallel_express': 0,
-        'parallel_fast_pixels': None,
-        'parallel_poisson_traps': False,
         'parallel_roe': {
-            'dwell_times': {
-                'array': [1.0],
-                'dtype': 'float64',
-                'type': 'numpy.ndarray'
-            },
-            'empty_traps_between_columns': True,
-            'empty_traps_for_first_transfers': False,
-            'force_release_away_from_readout': True,
             'type': 'arcticpy.src.roe.ROE',
-            'use_integer_express_matrix': False
+            'dwell_times': {
+                 'type': 'numpy.ndarray',
+                 'array': [1.0],
+                 'dtype': 'float64'},
+                 'empty_traps_between_columns': True,
+                  'empty_traps_for_first_transfers': False,
+                  'force_release_away_from_readout': True,
+                  'use_integer_express_matrix': False
         },
+       'parallel_express': 0, 
+        'parallel_offset': 0,
         'parallel_window_start': 0,
         'parallel_window_stop': -1,
-        'poisson_seed': -1,
-        'serial_express': 0,
-        'serial_fast_pixels': None,
+        'parallel_poisson_traps': False,
+        'parallel_fast_pixels': None,
         'serial_roe': {
+            'type': 'arcticpy.src.roe.ROE',
             'dwell_times': {
+                'type': 'numpy.ndarray',
                 'array': [1.0],
-                'dtype': 'float64',
-                'type': 'numpy.ndarray'
+                'dtype': 'float64'
             },
             'empty_traps_between_columns': True,
             'empty_traps_for_first_transfers': False,
             'force_release_away_from_readout': True,
-            'type': 'arcticpy.src.roe.ROE',
             'use_integer_express_matrix': False
         },
+        'serial_express': 0,
+        'serial_offset': 0,
         'serial_window_start': 0,
         'serial_window_stop': -1,
-        'type': 'autocti.clocker.two_d.Clocker2D',
-        'verbosity': 0
+        'serial_fast_pixels': None,
+        'verbosity': 0,
+        'poisson_seed': -1,
+        'euclid_orientation_hack': False
     }
 
 
