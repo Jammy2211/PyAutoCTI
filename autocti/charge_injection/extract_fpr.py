9bd0360e1d2702800e7a43b66429c40c84da7cf2
James Nightingale <james.w.nightingale@durham.ac.uk>
2022-06-07 15:41:07 +0100

    can estimate charge injections for entire columns

--- autocti/charge_injection/extract_fpr.py
+++ autocti/charge_injection/extract_fpr.py
@@ -19,13 +19,15 @@ class Extract2DParallelFPRCI(Extract2DParallelFPR):
         The inner regions of the FPR of each charge injection line informs us of the injected level of charge for that
         injection.
 
-        By taking the median of values after accounting for those which have had electrons captured and relocatred due
-        to CTI, we can therefore estimate the charge injeciton normalization.
+        By taking the median of values of the charge injection regions (after accounting for those which have had
+        electrons captured and relocated due to CTI), we can therefore estimate the input charge injection
+        normalization.
 
         This function does this for every column of every individual charge injection for every charge injection region.
-        For example, if there are 3 charge injection regions, this function returns a list of list where the outer
+
+        For example, if there are 3 charge injection regions, this function returns a list of lists where the outer
         list contains 3 lists each of which give estimates of the charge injection normalization in a given charge
-        injection region,
+        injection region. Thd size of the inner list is therefore the number of charge injection columns.
 
         The function `injection_normalization_list_from` performs the median over all charge injection region in each
         column and thus estimates a single injection normalization per column. Which function one uses depends on the
@@ -60,3 +62,54 @@ class Extract2DParallelFPRCI(Extract2DParallelFPR):
             injection_normalization_lists.append(injection_of_array_list)
 
         return injection_normalization_lists
+
+    def injection_normalization_list_from(
+        self, array: aa.Array2D, pixels: Tuple[int, int]
+    ) -> List[List]:
+        """
+        The inner regions of the FPR of each charge injection line informs us of the injected level of charge for that
+        injection.
+
+        By taking the median of values of the charge injection regions (after accounting for those which have had
+        electrons captured and relocated due to CTI), we can therefore estimate the input charge injection
+        normalization.
+
+        This function does this for every column of every charge injection. If multiple charge injections are performed
+        per column, the median of all charge regions is taken.
+
+        For example, if there are 3 charge injection regions, this function returns a list where each value
+        estimates the charge injection normalization of a given charge injection region. The size of this list
+        is therefore the number of charge injection columns.
+
+        The function `injection_normalization_lists_from` performs the median over each individual charge injection
+        region in each column. It therefore estimates multiple injection normalizations per column. for each individual
+        charge region. Which function one uses depends on the properties of the charge injection on the instrumentation.
+
+        Parameters
+        ----------
+        array
+            The charge injection image from which the charge injection normalizations are estimated.
+        pixels
+            The row pixel index to extract the FPR between (e.g. `pixels=(0, 3)` extracts the 1st, 2nd and 3rd
+            FPR rows). To remove the 10 leading pixels which have lost electrons due to CTI, an input such
+            as `pixels=(10, 30)` would be used.
+        """
+        injection_normalization_list = []
+
+        arr_list = [
+            np.ma.array(data=array.native[region.slice], mask=array.mask[region.slice])
+            for region in self.region_list_from(pixels=pixels)
+        ]
+
+        for column_index in range(arr_list[0].shape[1]):
+
+            for i, array_2d in enumerate(arr_list):
+
+                if i == 0:
+                    arr = array_2d[:, column_index]
+                else:
+                    arr = np.concatenate((arr[:], array_2d[:, column_index]))
+
+            injection_normalization_list.append(float(np.ma.median(arr)))
+
+        return injection_normalization_list
--- test_autocti/charge_injection/test_extract_fpr.py
+++ test_autocti/charge_injection/test_extract_fpr.py
@@ -35,3 +35,35 @@ def test__injection_normalization_lists_from(parallel_array, parallel_masked_arr
     )
     assert injection_normalization_lists[0] == [2.0, 2.0, 1.5]
     assert injection_normalization_lists[1] == [5.5, 6.0, 6.0]
+
+
+def test__injection_normalization_list_from(parallel_array, parallel_masked_array):
+
+    extract = ac.Extract2DParallelFPRCI(region_list=[(1, 4, 0, 3)])
+
+    # Extracts [1.0, 2.0, 3.0] of every injection in `parallel_array`
+
+    injection_normalization_list = extract.injection_normalization_list_from(
+        array=parallel_array, pixels=(0, 3)
+    )
+    assert injection_normalization_list == [2.0, 2.0, 2.0]
+
+    # Reduce pixels to only extract 1.0.
+
+    injection_normalization_list = extract.injection_normalization_list_from(
+        array=parallel_array, pixels=(0, 1)
+    )
+    assert injection_normalization_list == [1.0, 1.0, 1.0]
+
+    extract = ac.Extract2DParallelFPRCI(region_list=[(1, 4, 0, 3), (5, 8, 0, 3)])
+
+    injection_normalization_list = extract.injection_normalization_list_from(
+        array=parallel_array, pixels=(0, 3)
+    )
+    assert injection_normalization_list == [4.0, 4.0, 4.0]
+
+    injection_normalization_list = extract.injection_normalization_list_from(
+        array=parallel_masked_array, pixels=(0, 3)
+    )
+
+    assert injection_normalization_list == [2.5, 4.0, 3.5]
