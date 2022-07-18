8437f958ce0773449126dbfbd77b99e2dfe7bfaa
James Nightingale <james.w.nightingale@durham.ac.uk>
2022-07-18 14:31:47 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,8 @@ numpydoc_class_members_toctree = False
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
+os.system("python3 -m pip install --index-url https://test.pypi.org/simple --extra-index-url https://pypi.org/simple arcticpy")
+
 # -- Options for HTML output -------------------------------------------------
 
 # html_theme = "furo"
