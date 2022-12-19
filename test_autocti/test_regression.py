fb000b68f942a92d8a6ca64e29709b41976d5be4
Jammy2211 <james.w.nightingale@durham.ac.uk>
2022-12-19 16:44:42 +0000

    remove failing test

--- test_autocti/test_regression.py
+++
@@ -1,6 +0,0 @@
-model = af.Collection(
-    cti=af.Model(ac.CTI2D, parallel_trap_list=traps_x1, parallel_ccd=ccd),
-    hyper_noise=af.Model(ac.HyperCINoiseCollection),
-)
-
-print(model.has(ac.HyperCINoiseCollection))
