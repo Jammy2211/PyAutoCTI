be2e8bcc05c2a75f22d8df1d1efeab0e280439a8
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-04-06 17:24:39 +0100

    remoe over testing in ImagingCIVisualizer

--- test_autocti/charge_injection/model/test_visualizer_ci.py
+++ test_autocti/charge_injection/model/test_visualizer_ci.py
@@ -30,9 +30,6 @@ def test__visualizes_imaging_ci_using_configs(imaging_ci_7x7, plot_path, plot_pa
     assert path.join(plot_path, "subplot_imaging_ci.png") in plot_patch.paths
     assert path.join(plot_path, "image_2d.png") in plot_patch.paths
     assert path.join(plot_path, "noise_map.png") not in plot_patch.paths
-    assert path.join(plot_path, "signal_to_noise_map.png") not in plot_patch.paths
-    assert path.join(plot_path, "pre_cti_data.png") in plot_patch.paths
-    assert path.join(plot_path, "cosmic_ray_map.png") in plot_patch.paths
 
 
 def test__visualizes_imaging_ci_regions_using_configs(
@@ -53,11 +50,6 @@ def test__visualizes_imaging_ci_regions_using_configs(
     assert path.join(plot_path, "subplot_1d_ci_parallel_fpr.png") in plot_patch.paths
     assert path.join(plot_path, "image_parallel_fpr.png") in plot_patch.paths
     assert path.join(plot_path, "noise_map_parallel_fpr.png") not in plot_patch.paths
-    assert (
-        path.join(plot_path, "signal_to_noise_map_parallel_fpr.png")
-        not in plot_patch.paths
-    )
-    assert path.join(plot_path, "pre_cti_data_parallel_fpr.png") in plot_patch.paths
 
 
 def test___visualizes_fit_ci_using_configs(fit_ci_7x7, plot_path, plot_patch):
@@ -74,12 +66,6 @@ def test___visualizes_fit_ci_using_configs(fit_ci_7x7, plot_path, plot_patch):
     assert path.join(plot_path, "subplot_fit_ci.png") in plot_patch.paths
     assert path.join(plot_path, "image_2d.png") in plot_patch.paths
     assert path.join(plot_path, "noise_map.png") not in plot_patch.paths
-    assert path.join(plot_path, "signal_to_noise_map.png") not in plot_patch.paths
-    assert path.join(plot_path, "pre_cti_data.png") in plot_patch.paths
-    assert path.join(plot_path, "post_cti_data.png") in plot_patch.paths
-    assert path.join(plot_path, "residual_map.png") not in plot_patch.paths
-    assert path.join(plot_path, "normalized_residual_map.png") in plot_patch.paths
-    assert path.join(plot_path, "chi_squared_map.png") in plot_patch.paths
 
     plot_patch.paths = []
 
@@ -88,12 +74,6 @@ def test___visualizes_fit_ci_using_configs(fit_ci_7x7, plot_path, plot_patch):
     assert path.join(plot_path, "subplot_fit_ci.png") in plot_patch.paths
     assert path.join(plot_path, "image_2d.png") in plot_patch.paths
     assert path.join(plot_path, "noise_map.png") in plot_patch.paths
-    assert path.join(plot_path, "signal_to_noise_map.png") in plot_patch.paths
-    assert path.join(plot_path, "pre_cti_data.png") in plot_patch.paths
-    assert path.join(plot_path, "post_cti_data.png") in plot_patch.paths
-    assert path.join(plot_path, "residual_map.png") in plot_patch.paths
-    assert path.join(plot_path, "normalized_residual_map.png") in plot_patch.paths
-    assert path.join(plot_path, "chi_squared_map.png") in plot_patch.paths
 
 
 def test___visualizes_fit_ci_regions_using_configs(fit_ci_7x7, plot_path, plot_patch):
@@ -114,18 +94,6 @@ def test___visualizes_fit_ci_regions_using_configs(fit_ci_7x7, plot_path, plot_p
     )
     assert path.join(plot_path, "image_parallel_fpr.png") in plot_patch.paths
     assert path.join(plot_path, "noise_parallel_fpr.png") not in plot_patch.paths
-    assert (
-        path.join(plot_path, "signal_to_noise_map_parallel_fpr.png")
-        not in plot_patch.paths
-    )
-    assert path.join(plot_path, "pre_cti_data_parallel_fpr.png") in plot_patch.paths
-    assert path.join(plot_path, "post_cti_data_parallel_fpr.png") in plot_patch.paths
-    assert path.join(plot_path, "residual_map_parallel_fpr.png") not in plot_patch.paths
-    assert (
-        path.join(plot_path, "normalized_residual_map_parallel_fpr.png")
-        in plot_patch.paths
-    )
-    assert path.join(plot_path, "chi_squared_map_parallel_fpr.png") in plot_patch.paths
 
     plot_patch.paths = []
 
@@ -138,17 +106,6 @@ def test___visualizes_fit_ci_regions_using_configs(fit_ci_7x7, plot_path, plot_p
     )
     assert path.join(plot_path, "image_parallel_fpr.png") in plot_patch.paths
     assert path.join(plot_path, "noise_map_parallel_fpr.png") in plot_patch.paths
-    assert (
-        path.join(plot_path, "signal_to_noise_map_parallel_fpr.png") in plot_patch.paths
-    )
-    assert path.join(plot_path, "pre_cti_data_parallel_fpr.png") in plot_patch.paths
-    assert path.join(plot_path, "post_cti_data_parallel_fpr.png") in plot_patch.paths
-    assert path.join(plot_path, "residual_map_parallel_fpr.png") in plot_patch.paths
-    assert (
-        path.join(plot_path, "normalized_residual_map_parallel_fpr.png")
-        in plot_patch.paths
-    )
-    assert path.join(plot_path, "chi_squared_map_parallel_fpr.png") in plot_patch.paths
 
 
 def test__visualize_fit_ci_combined(fit_ci_7x7, plot_path, plot_patch):
@@ -166,12 +123,9 @@ def test__visualize_fit_ci_combined(fit_ci_7x7, plot_path, plot_patch):
 
     assert path.join(plot_path, "subplot_residual_map_list.png") in plot_patch.paths
     assert (
-        path.join(plot_path, "subplot_normalized_residual_map_list.png")
-        in plot_patch.paths
-    )
-    assert (
         path.join(plot_path, "subplot_chi_squared_map_list.png") not in plot_patch.paths
     )
+
 
 def test__visualize_fit_ci_regions_combined(fit_ci_7x7, plot_path, plot_patch):
 
@@ -181,14 +135,20 @@ def test__visualize_fit_ci_regions_combined(fit_ci_7x7, plot_path, plot_patch):
     visualizer = VisualizerImagingCI(visualize_path=plot_path)
 
     visualizer.visualize_fit_ci_1d_regions_combined(
-        fit_list=[fit_ci_7x7, fit_ci_7x7], region_list=["parallel_fpr"], during_analysis=True
+        fit_list=[fit_ci_7x7, fit_ci_7x7],
+        region_list=["parallel_fpr"],
+        during_analysis=True,
     )
 
     plot_path = path.join(plot_path, "fit_imaging_ci_combined")
 
     assert (
-        path.join(plot_path, "subplot_data_with_noise_map_model_parallel_fpr_list.png") in plot_patch.paths
+        path.join(plot_path, "subplot_data_with_noise_map_model_parallel_fpr_list.png")
+        in plot_patch.paths
     )
     assert (
-        path.join(plot_path, "subplot_data_with_noise_map_model_logy_parallel_fpr_list.png") not in plot_patch.paths
+        path.join(
+            plot_path, "subplot_data_with_noise_map_model_logy_parallel_fpr_list.png"
+        )
+        not in plot_patch.paths
     )
